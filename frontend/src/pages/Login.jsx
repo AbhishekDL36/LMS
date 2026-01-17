321d706edba5eb04fbaa3c69ee75a036dcfcbca8
Abhishek-dice <abhishek@dice-academy.com>
2026-01-17 15:21:46 +0530

    assignment and test reformation

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -72,7 +72,7 @@ const Login = () => {
         {error && (
           <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
             {error}
-          </div>
+          </div> 
         )}
 
         {/* Login form */}
